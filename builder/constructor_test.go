204a4328eb8b9986002b16138f41c661dfa6d43d
Pavel Shvedko <pshvedko@gmail.com>
2026-02-14 08:08:44 +0300

    +1

--- builder/constructor_test.go
+++ builder/constructor_test.go
@@ -315,29 +315,30 @@ func TestConstructor_Delete(t *testing.T) {
 			args: args{
 				j: &o,
 				f: filter.Eq{"id": uuid.UUID{}},
-				o: []request.Option{request.WithField{"id", "time_1"}, request.WithReturnField{"id"}, request.WithoutReturning()},
+				o: []request.Option{request.WithField{"id"}},
 			},
-			want:    `DELETE FROM "objects" AS "o" WHERE "o"."id" = $1 RETURNING 1`,
+			want:    `DELETE FROM "objects" AS "o" WHERE "o"."id" = $1 RETURNING "o"."id"`,
 			want1:   []any{uuid.UUID{}},
-			want2:   []any{new(int64)},
+			want2:   []any{&o.ID},
 			wantErr: nil,
 		},
-		//{
-		//	name: "",
-		//	args: args{
-		//		j: &o,
-		//		f: nil,
-		//		o: []request.Option{
-		//			request.WithCreated("time_1"),
-		//			request.WithUpdated("time_2"),
-		//			request.WithDeleted("time_4"),
-		//		},
-		//	},
-		//	want:    `DELETE FROM "objects" AS "o" RETURNING "o"."id", "o"."uuid_2", "o"."uuid_3", "o"."uuid_4", "o"."bool_1", "o"."bool_2", "o"."bool_3", "o"."bool_4", "o"."float_32", "o"."float_64", "o"."int_8", "o"."int_16", "o"."int_32", "o"."int_64", "o"."string_1", "o"."string_2", "o"."string_3", "o"."string_4", "o"."time_1", "o"."time_2", "o"."time_3", "o"."time_4"`,
-		//	want1:   nil,
-		//	want2:   nil,
-		//	wantErr: nil,
-		//},
+		{
+			name: "",
+			args: args{
+				j: &o,
+				f: filter.Eq{"id": uuid.UUID{}},
+				o: []request.Option{
+					request.WithCreated("time_1"),
+					request.WithUpdated("time_2"),
+					request.WithDeleted("time_4"),
+					request.WithField{"id"},
+				},
+			},
+			want:    `UPDATE "objects" AS "o" SET "time_2" = DEFAULT, "time_4" = NOW() WHERE ( "o"."id" = $1 AND "o"."time_4" IS NOT NULL ) RETURNING "o"."id"`,
+			want1:   nil,
+			want2:   nil,
+			wantErr: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
