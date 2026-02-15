321bca68418482172d2f7ea2dea12c63da85aa78
Pavel Shvedko <pshvedko@gmail.com>
2026-02-15 13:46:43 +0300

    +1

--- dbx.go
+++ dbx.go
@@ -135,3 +135,7 @@ func (a *FlatArray[T]) Inject(j filter.Projector) {
 }
 
 func (a *FlatArray[T]) Cleanup() { *a = (*a)[:0] }
+
+func (a *FlatArray[T]) Load(ctx context.Context, db request.Connector, f filter.Filter, o, l *uint, y []string, oo ...request.Option) (uint, error) {
+	return List(ctx, db, a, f, o, l, y, oo...)
+}
