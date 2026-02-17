abdb5a69d9e7df6fb9d789d796e102a6dc83515b
Pavel Shvedko <pshvedko@gmail.com>
2026-02-17 18:45:54 +0300

    +1

--- builder/constructor_test.go
+++ builder/constructor_test.go
@@ -542,8 +542,9 @@ func BenchmarkConstructor_Select(b *testing.B) {
 			b.Fatal(err)
 		}
 		switch q {
-		// case `SELECT "o"."id", "o"."uuid_2", "o"."uuid_3", "o"."uuid_4", "o"."bool_1", "o"."bool_2", "o"."bool_3", "o"."bool_4", "o"."float_32", "o"."float_64", "o"."int_8", "o"."int_16", "o"."int_32", "o"."int_64", "o"."string_1", "o"."string_2", "o"."string_3", "o"."string_4", "o"."time_1", "o"."time_2", "o"."time_3", "o"."time_4" FROM "objects" AS "o" WHERE ( ( ( "o"."bool_2" IS TRUE AND "o"."int_8" = $1 ) AND ( "o"."float_32" = ANY($2) AND "o"."int_16" = ANY($3) AND "o"."string_1" = ANY($4) ) AND ( "o"."int_64" > $5 OR "o"."string_2" NOT LIKE $6 ) ) AND "o"."time_4" IS NULL ) ORDER BY 1 DESC, "o"."time_1" OFFSET $7 LIMIT $8`:
+		case `SELECT "o"."id", "o"."uuid_2", "o"."uuid_3", "o"."uuid_4", "o"."bool_1", "o"."bool_2", "o"."bool_3", "o"."bool_4", "o"."float_32", "o"."float_64", "o"."int_8", "o"."int_16", "o"."int_32", "o"."int_64", "o"."string_1", "o"."string_2", "o"."string_3", "o"."string_4", "o"."time_1", "o"."time_2", "o"."time_3", "o"."time_4" FROM "objects" AS "o" WHERE ( ( ( "o"."bool_2" IS TRUE AND "o"."int_8" = $1 ) AND ( "o"."float_32" = ANY($2) AND "o"."int_16" = ANY($3) AND "o"."string_1" = ANY($4) ) AND ( "o"."int_64" > $5 OR "o"."string_2" NOT LIKE $6 ) ) AND "o"."time_4" IS NULL ) ORDER BY 1 DESC, "o"."time_1" OFFSET $7 LIMIT $8`:
+		default:
+			b.Fatal(q)
 		}
 	}
-	// b.Log(q)
 }
